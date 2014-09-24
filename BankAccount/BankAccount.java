9c50d940a60f4086b9371019285f71693d473598
njason7 <n.jason7@gmail.com>
2014-09-24 13:20:30 -0400

    Update BankAccount.java

--- BankAccount/BankAccount.java
+++ BankAccount/BankAccount.java
@@ -1,6 +1,12 @@
 public class BankAccount{
     private int balance,account,pin;
-    private String user,pass;
+    private String user,pass,all;
+    public BankAccount(int balance, int account, String user, String pass, int pin){
+	setbalance(balance);
+	setaccount(account);
+	setuser(user);
+	setpass(pass);
+	setpin(pin);}
     public void setbalance(int balance){
 	this.balance = balance;}
     public void setaccount(int account){
@@ -11,4 +17,17 @@ public class BankAccount{
 	this.user = user;}
     public void setpass(int pass){
 	this.pass = pass;}
+    public void deposit(int dep){
+	balance = balance + dep;}
+    public void withdraw(int with){
+	if (balance >= with){
+	    balance = balance - with;
+	    return true}
+	else {
+	    return false}}
+    public String print(){
+	return balance+" "+account+" "+user+" "+pass+" "+pin;}
+    public String toString(){
+	return balance+"
+    
 }
