bc9624c5bf7da9347fdc29ce30b65d232e2ad8de
njason7 <n.jason7@gmail.com>
2014-09-24 13:00:29 -0400

    Create BankAccount.java

---
+++ BankAccount/BankAccount.java
@@ -0,0 +1,14 @@
+public class BankAccount{
+    private int balance,account,pin;
+    private String user,pass;
+    public void setbalance(int balance){
+	this.balance = balance;}
+    public void setaccount(int account){
+	this.account = account;}
+    public void setpin(int pin){
+	this.pin = pin;}
+    public void setuser(int user){
+	this.user = user;}
+    public void setpass(int pass){
+	this.pass = pass;}
+}
