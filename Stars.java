3e27a794a223e661f7d4b058fb829fa2f5e3a7df
njason7 <n.jason7@gmail.com>
2014-10-01 13:18:57 -0400

    Create Stars.java

---
+++ Stars.java
@@ -0,0 +1,36 @@
+public class Stars{
+    public static String rect(int len, int wid){
+	int countl=0;
+	String fin ="";
+	while (countl < len){
+	    int countw=0;
+	    while (countw < wid){
+		fin+="*";
+		countw++;
+	    }
+	    countl++;
+	    fin+="\n";
+	}
+	return fin;
+    }
+    public static String tri(int s){
+	int count=0;
+	String str="";
+	while (count<s){
+	    int count2=0;
+	    while (count2<=count){
+		str+="*";
+		count2++;
+	    }
+	    str+="\n";
+	    count++;
+	}
+	return str;
+    }
+    public static String triBack(int s){
+	int
+    public static void main (String[]args){
+	System.out.println(rect(2,3));
+	System.out.println(tri(3));
+    }
+}
