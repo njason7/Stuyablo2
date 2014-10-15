dd3b7f393c59ff4678344ac7e0a7f571bf774343
njason7 <n.jason7@gmail.com>
2014-10-15 18:42:58 -0400

    Update Wizard.java
    Added hit and damage

--- Inheritance/Wizard.java
+++ Inheritance/Wizard.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 public class Wizard extends Adventurer{
     private int mana;
     public Wizard(){
@@ -9,7 +10,7 @@ public class Wizard extends Adventurer{
     public Wizard(String s, int startHP){
 	super(s,startHP);
 	setMana(20);
-	setINT(7);
+	setINT(13);
     }
     public void setMana(int mana){
 	this.mana = mana;
@@ -18,9 +19,38 @@ public class Wizard extends Adventurer{
 	return mana;
     }
     public void attack(Adventurer other){
-	System.out.println(this+" fires a bolt against "+other);
+	Random hitc = new Random();
+	Random hitdmg = new Random();
+	int dmg;
+	if (this.getDEX() > other.getDEX()){
+	    dmg = hitdmg.nextInt(3) + this.getSTR() - 7;
+	    System.out.println(this+" poked "+other+" and did "+dmg+" damage.");
+	}else if ((this.getDEX() / other.getDEX()) > hitc.nextDouble()){
+	    dmg = hitdmg.nextInt(3) + this.getSTR() - 7;
+	    System.out.println(this+" poked "+other+" and did "+dmg+" damage.");
+	}else{
+	    System.out.println(this+" tried to poke "+other+" and missed.");
+	    dmg = 0;
+	}
+	other.setHP(other.getHP() - dmg);
+	dmg = 0;
     }
     public void special(Adventurer other){
-	System.out.println(this+" casts Ultima against "+other);
+	Random hitc = new Random();
+	Random sphitdmg = new Random();
+	int spdmg;
+	if (this.getINT() > other.getDEX()-2){
+	    spdmg = sphitdmg.nextInt(3) + this.getINT() + 3;
+	    System.out.println(this+" casted Fire against "+other+" and did "+spdmg+" damage.");
+	}else if ((this.getINT() / (other.getDEX()-2)) > hitc.nextDouble()){
+	    spdmg = sphitdmg.nextInt(3) + this.getINT() + 3;
+	    System.out.println(this+" casted Fire against "+other+" and did "+spdmg+" damage.");
+	}else{
+	    System.out.println(this+" attempted to cast Fire against "+other+" and missed.");
+	    spdmg = 0;
+	}
+	other.setHP(other.getHP() - spdmg);
+	spdmg = 0;
+	setMana(mana - 5);
     }
 }
