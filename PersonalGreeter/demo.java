9a22162162bb87254d0105b2532895dcc32f8d4a
njason7 <n.jason7@gmail.com>
2014-10-31 00:27:16 -0400

    Delete demo.java

--- PersonalGreeter/demo.java
+++
@@ -1,9 +0,0 @@
-public class demo{
-    public static void main(String[]args){
-	PersonalGreeter Bob,Mary,Juan;
-	Bob = new PersonalGreeter();
-	Mary = new PersonalGreeter("Mary");
-	Juan = new PersonalGreeter("Juan","Hola");
-	System.out.println(Bob.greet());
-	System.out.println(Mary.greet());
-	System.out.println(Juan.greet());}}
